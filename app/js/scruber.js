12f65b3e4ca769ec3f1d1663dc266203e8df1230
Greg Markou <gregorymarkou@gmail.com>
2017-10-17 20:48:28 -0400

    added sudo privilages prompt

--- app/js/scruber.js
+++ app/js/scruber.js
@@ -1,19 +1,23 @@
 const $ = require('jQuery');
 const shell = require('shelljs');
-
+var sudo = require('sudo-prompt');
+var options = {
+  name: 'eth drive'
+};
 // FAST SCRUB OPTION
 function fastScrub(deviceName, callback) {
     $('.drive-list-container').html("<div>Formating....</div>");        
-    shell.exec(`sudo ./format-udf.sh -w quick ${deviceName} 'eth_wallet'`, function(code, stdout, stderr) {
-        console.log('Exit code:', code);
+    // shell.exec(`sudo ./format-udf.sh -w quick ${deviceName} 'eth_wallet'`, function(code, stdout, stderr) {
+    sudo.exec(`./format-udf.sh -w quick ${deviceName} 'eth_wallet'`, options, function(error, stdout, stderr) {
+        console.log('Exit code:', error);
         console.log('Program output:', stdout);
         console.log('Program stderr:', stderr);
-        if (code === 0) {
-            $('.filesizes-container').html("<div>" + "\n" + "Your drive has been formatted!</div>");        
+        // if (code === 0) {
+            // $('.filesizes-container').html("<div>" + "\n" + "Your drive has been formatted!</div>");        
             callback(true);
-        } else {
-            callback(false);
-        }
+        // } else {
+            // callback(false);
+        // }
     })
 }
 
