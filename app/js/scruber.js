3a284b75d3efde4ae47932627db9a1bfbe40f0f1
Greg Markou <gregorymarkou@gmail.com>
2017-10-17 20:55:15 -0400

    added error handling for new shell exec

--- app/js/scruber.js
+++ app/js/scruber.js
@@ -1,5 +1,4 @@
 const $ = require('jQuery');
-const shell = require('shelljs');
 var sudo = require('sudo-prompt');
 var options = {
   name: 'eth drive'
@@ -7,28 +6,32 @@ var options = {
 // FAST SCRUB OPTION
 function fastScrub(deviceName, callback) {
     $('.drive-list-container').html("<div>Formating....</div>");        
-    // shell.exec(`sudo ./format-udf.sh -w quick ${deviceName} 'eth_wallet'`, function(code, stdout, stderr) {
     sudo.exec(`./format-udf.sh -w quick ${deviceName} 'eth_wallet'`, options, function(error, stdout, stderr) {
-        console.log('Exit code:', error);
+        console.log('Error:', error);
         console.log('Program output:', stdout);
         console.log('Program stderr:', stderr);
-        // if (code === 0) {
-            // $('.filesizes-container').html("<div>" + "\n" + "Your drive has been formatted!</div>");        
+        if (error === null) {
+            $('.filesizes-container').html("<div>Your drive has been formatted!</div>");        
             callback(true);
-        // } else {
-            // callback(false);
-        // }
+        } else {
+            $('.filesizes-container').html(`<div>An error occured: \n ${error}</div>`);  
+            callback(false);
+        }
     })
 }
 
 // SLOW SCRUB OPTION
 function deepScrub(deviceName) {
     if (selectedDrive !== '') {
-        shell.exec(`./format-udf.sh -w zero ${deviceName} 'eth_wallet'`, function(code, stdout, stderr) {
+        sudo.exec(`./format-udf.sh -w zero ${deviceName} 'eth_wallet'`, options, function(error, stdout, stderr) {
             console.log('Exit code:', code);
             console.log('Program output:', stdout);
-            if (code === 0) {
-                $('.filesizes-container').html("<div>Format success!" + "\n" + "Your drive has been formatted!</div>");        
+            if (error === null) {
+                $('.filesizes-container').html("<div>Your drive has been scrubbed!</div>");        
+                callback(true);
+            } else {
+                $('.filesizes-container').html(`<div>An error occured: \n ${error}</div>`);  
+                callback(false);
             }
         });
     }
